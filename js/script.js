bc36ef2688c526cace0e1754ad3d02bc88c5b3e8
Lykaiossoul <lore.orellana.b@gmail.com>
2026-03-10 19:05:49 -0300

    buscador agregado

--- js/script.js
+++ js/script.js
@@ -1,99 +1,6 @@
-// const dragones = [
-//     {
-//         id: 1,
-//         nombre: "Skynight",
-//         elemento: "Oscuridad",
-//         rareza: "Legendario",
-//     },
-//     {
-//         id: 2,
-//         nombre: "Frostbite",
-//         elemento: "Hielo",
-//         rareza: "Raro",
-//     },
-//     {
-//         id: 3,
-//         nombre: "Sorana",
-//         elemento: "Aire",
-//         rareza: "Comun",
-//     },
-//     {
-//         id: 4,
-//         nombre: "Aobi",
-//         elemento: "Fuego",
-//         rareza: "Epico",
-//     },
-//     {
-//         id: 5,
-//         nombre: "Hikari",
-//         elemento: "Luz",
-//         rareza: "Poco comun",
-//     },
-//     {
-//         id: 6,
-//         nombre: "Ikazuchi",
-//         elemento: "Electrico",
-//         rareza: "Comun",
-//     },
-//     {
-//         id: 7,
-//         nombre: "Shinme",
-//         elemento: "Tierra",
-//         rareza: "Raro",
-//     },
-//     {
-//         id: 8,
-//         nombre: "Kuro",
-//         elemento: "Oscuridad",
-//         rareza: "Poco comun",
-//     },
-//     {
-//         id: 9,
-//         nombre: "Syrcasca",
-//         elemento: "Agua",
-//         rareza: "Epico",
-//     },
-//     {
-//         id: 10,
-//         nombre: "Vulcanus",
-//         elemento: "Fuego",
-//         rareza: "Raro",
-//     },
-//     {
-//         id: 11,
-//         nombre: "Aurora",
-//         elemento: "Hielo",
-//         rareza: "Legendario",
-//     },
-//     {
-//         id: 12,
-//         nombre: "Nimara",
-//         elemento: "Agua",
-//         rareza: "Comun",
-//     },
-//     {
-//         id: 13,
-//         nombre: "Ygdrasil",
-//         elemento: "Tierra",
-//         rareza: "Legendario",
-//     },
-//     {
-//         id: 14,
-//         nombre: "Thunderclaw",
-//         elemento: "Electrico",
-//         rareza: "Epico",
-//     },
-//     {
-//         id: 15,
-//         nombre: "Zpyra",
-//         elemento: "Luz",
-//         rareza: "Comun",
-//     },
-// ]
 
 const jsonCriaturas = "./db/data.json";
 let criaturas = [];
-let coleccion = JSON.parse(localStorage.getItem("registroDeColeccion")) || [];
 let contenedorCriaturas = document.getElementById("contenedor-criaturas");
 let buscadorInput = document.getElementById("buscador-criaturas");
 
@@ -178,6 +85,14 @@ function actualizacionDeObservaciones(idDeCriatura) {
     localStorage.setItem("registroDeColeccion", JSON.stringify(criaturas))
     renderizarCriaturas(criaturas);
 }
+
+buscadorInput.addEventListener("input", function () {
+    let textoBuscado = buscadorInput.value;
+    let resultadoDeBusqueda = criaturas.filter(function (criatura) {
+        return criatura.nombre.includes(textoBuscado);
+    })
+    renderizarCriaturas(resultadoDeBusqueda);
+})
 
 
 // //if usando .classList.add()
